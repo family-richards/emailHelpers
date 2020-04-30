ec71871c6ba564ab0645724aa1e80753562790e7
kendell <KTibow@users.noreply.github.com>
2020-04-30 13:59:22 -0700

    Update test.py

--- test.py
+++ test.py
@@ -2,3 +2,5 @@ from os import getenv
 def test_make_email():
   from emailHelpers import Email
   email = Email("kidscodingplace@gmail.com")
+  email.setTo("kidscodingplace@gmail.com")
+  email.setSubject("Does this thing work?")
