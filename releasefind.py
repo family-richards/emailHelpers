36d4c1422325eb10c3751170471576d6e59174fb
kendell <KTibow@users.noreply.github.com>
2020-04-30 08:42:16 -0700

    Update releasefind.py

--- releasefind.py
+++ releasefind.py
@@ -3,3 +3,9 @@ from os import getenv
 g = Github(getenv("token"))
 rels = g.get_user().get_repo("Python-Email-Helpers").get_releases()
 tag = rels[rels.totalCount-1].tag_name
+setup = open("setup.py", "r").read()
+setup = setup.split("\n")
+setup[1] = 'version = "'+tag+'"'
+setupW = open("setup.py", "w")
+setupW.write("\n".join(setup))
+setupW.close()
