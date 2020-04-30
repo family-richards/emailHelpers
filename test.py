edf33fbc1e7e7c83d43808ed5bb43e30682df6ed
kendell <KTibow@users.noreply.github.com>
2020-04-30 14:54:57 -0700

    Update test.py

--- test.py
+++ test.py
@@ -2,8 +2,8 @@ from os import getenv
 def test_make_email():
   from emailHelpers import Email
   email = Email("kidscodingplace@gmail.com")
-  email.setTo("kidscodingplace@gmail.com")
-  email.setSubject("Does this thing work?")
+  email.set_to("kidscodingplace@gmail.com")
+  email.set_subject("Does this thing work?")
   emailstr = email.as_string()
 
 def test_add_attatchment():
@@ -11,5 +11,5 @@ def test_add_attatchment():
   email = Email("kidscodingplace@gmail.com")
   with open("test.txt", "w") as t:
     t.write("testing...")
-  email.addAttachmentFromFile("test.txt")
-  email.addMyselfToEmail()
+  email.add_attachment_from_file("test.txt")
+  email.add_myself_to_email()
