e9c8b7d62ea5a4d7b2623667dba0488132a8a3d6
kendell <KTibow@users.noreply.github.com>
2020-04-30 09:56:15 -0700

    Update releasefind.py

--- releasefind.py
+++ releasefind.py
@@ -5,7 +5,7 @@ g = Github(getenv("token"))
 print("Waiting for GitHub")
 sleep(10)
 rels = g.get_organization("family-richards").get_repo("Python-Email-Helpers").get_releases()
-tag = rels[rels.totalCount-1].tag_name
+tag = rels[0].tag_name
 print("Latest version is "+tag)
 setup = open("setup.py", "r").read()
 setup = setup.split("\n")
