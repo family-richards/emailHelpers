e42af4c8cc94fd5a4c48ad305c080fb23e425175
kendell <KTibow@users.noreply.github.com>
2020-04-30 09:41:17 -0700

    Update releasefind.py

--- releasefind.py
+++ releasefind.py
@@ -4,7 +4,7 @@ from time import sleep
 g = Github(getenv("token"))
 print("Waiting for GitHub")
 sleep(10)
-rels = g.get_user().get_repo("Python-Email-Helpers").get_releases()
+rels = g.get_organization("family-richards").get_repo("Python-Email-Helpers").get_releases()
 tag = rels[rels.totalCount-1].tag_name
 print("Latest version is "+tag)
 setup = open("setup.py", "r").read()
