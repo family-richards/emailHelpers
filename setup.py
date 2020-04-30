957ae4efdb3c186b50139c7a65af096a0435017d
kendell <KTibow@users.noreply.github.com>
2020-04-29 19:10:59 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
-print("Going to install...")
-from distutils.core import setup
-print("Imported setup tool...")
-setup(name="emailHelpers",
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(name="emailHelpers-KTibow",
       version='1.0',
       packages=['emailHelpers'],
       author="Kendell R.",
-      author_email="ejrejr@hotmail.com",
+      author_email="kidscodingplace@gmail.com",
       license="MIT",
       description="Two classes to help with managing emails.",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       download_url="https://github.com/family-richards/Python-Email-Helpers",
-      url="https://github.com/family-richards/Python-Email-Helpers")
-print("Package installed!")
+      url="https://github.com/family-richards/Python-Email-Helpers",
+      packages=setuptools.find_packages(),
+      python_requires='>=3.0')
