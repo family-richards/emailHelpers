ff95a26f7415682d3ed4d7841a1e3c438db5793f
kendell <KTibow@users.noreply.github.com>
2020-04-30 12:56:59 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("PYPI.md", "r") as fh:
 with open("requirements.txt", "w") as fh:
     fh.write("\n".join(setuptools.find_packages()))
 setuptools.setup(name="emailHelpers",
-      version=version,
-      author="Kendell R.",
-      author_email="kidscodingplace@gmail.com",
-      license="MIT",
-      description="emailHelpers is a wrapper for the smtplib and email packages.",
-      long_description=long_description,
-      long_description_content_type="text/markdown",
-      download_url="https://github.com/family-richards/Python-Email-Helpers",
-      url="https://github.com/family-richards/Python-Email-Helpers",
-      packages=setuptools.find_packages(),
-      python_requires='>=3.0')
+        version=version,
+        author="Kendell R.",
+        author_email="kidscodingplace@gmail.com",
+        license="MIT",
+        description="emailHelpers is a wrapper for the smtplib and email packages.",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        download_url="https://github.com/family-richards/Python-Email-Helpers",
+        url="https://github.com/family-richards/Python-Email-Helpers",
+        packages=setuptools.find_packages(),
+        python_requires='>=3.0')
