c77fd64c10f62de79f0d3c35fe69dfdecdbd60c1
kendell <KTibow@users.noreply.github.com>
2020-06-03 15:29:22 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,4 +13,8 @@ setup(name="emailHelpers",
                  url="https://family-richards.github.io/emailHelpers/",
                  packages=find_packages(),
                  classifiers=["Topic :: Communications :: Email", "Natural Language :: English", "Operating System :: OS Independent", "Programming Language :: Python :: 3 :: Only", "Topic :: Software Development :: Libraries :: Python Modules", "Development Status :: 5 - Production/Stable", "License :: OSI Approved :: MIT License"],
-                 python_requires='>=3.0')
+                 python_requires='>=3.0',
+                 project_urls={
+                     "Tracker", "https://github.com/family-richards/emailHelpers/issues?q=is%3Aissue+is%3Aopen+sort%3Aupdated-desc",
+                     "Source", "https://github.com/family-richards/emailHelpers/"
+                 })
