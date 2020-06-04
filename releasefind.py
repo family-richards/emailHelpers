00c4118ba6a94636397777123cdb0c6623649b3e
kendell <KTibow@users.noreply.github.com>
2020-06-04 11:42:02 -0700

    Update releasefind.py

--- releasefind.py
+++ releasefind.py
@@ -7,6 +7,8 @@ print("Done waiting...")
 g = Github(getenv("token"))
 rels = g.get_organization("family-richards").get_repo("emailHelpers").get_releases()
 tag = rels[0].tag_name.replace("v", "")
+if tag[0] == 'z':
+  tag = rels[1].tag_name.replace("v", "")
 print("Latest version is "+tag)
 setup = open("setup.py", "r").read()
 setup = setup.split("\n")
